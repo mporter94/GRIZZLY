9f3e072461347229cb6ef3c4e7b795c9a8b349a8
The_Porter_Rico <mporter94@gmail.com>
2021-11-04 15:55:52 +0100

    Delete admin.php

--- admin.php
+++
@@ -1,8 +0,0 @@
-<!---ADMIN PAGE--->
-<script>
-    $(document).ready(function() {
-        $(document).ready(function() {
-            new adminPage({container: $(`#page`)}).html;
-        });
-    });
-</script>
